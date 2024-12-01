839354f111034b04610f04f89e638d41adac53a3
Heriberto-Juarez <heribertojuarezj@gmail.com>
2024-11-30 22:50:36 -0600

    chore: add log

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
@@ -8,7 +8,10 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-  await app.listen(process.env.PORT ?? 3001);
+  const port = 3001;
+  await app.listen(process.env.PORT ?? port);
+
+  new Logger().log(`App running on port ${port}`);
 }
 
 bootstrap();
